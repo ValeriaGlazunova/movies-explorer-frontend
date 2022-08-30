6b6e3d335bdebda29ddd84c0fe342ac0d1ca59e4
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-08-30 22:26:46 +0500

    добавить верстку поисковика фильмов

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,22 +1,26 @@
 import React from "react";
-import './SearchForm.css';
-import icon from '../../images/icon.svg';
+import "./SearchForm.css";
+import icon from "../../images/icon.svg";
 
 export default function SearchForm() {
-    return (
-        <section className="search-form">
-            <div className="search-form__container">
-                <img className="search-form__icon" alt="icon" src={icon} />
-                <form className="serch-form__line">
-                <input className="search-form__input" placeholder="Фильм" />
-                <button className="search-form__button" type="button"></button>
-                <label className="search-form__checkbox">
-                    <input className="search-form__checkbox__input" type="checkbox" />
-                <span className="search-form__checkbox__pseudo-element"></span>
-                Короткометражки
-                </label>
-                </form>
-            </div>
-        </section>
-    )
+  return (
+    <section className="search-form">
+      <div className="search-form__container">
+        <img className="search-form__icon" alt="icon" src={icon} />
+        <form className="serch-form__line">
+          <input className="search-form__input" placeholder="Фильм" />
+          <button className="search-form__button" type="button"></button>
+          <label className="search-form__checkbox" htmlFor="checkbox">
+            <input
+              className="search-form__checkbox__input"
+              type="checkbox"
+              name="checkbox"
+            />
+            <span className="search-form__checkbox__pseudo-el"></span>
+            Короткометражки
+          </label>
+        </form>
+      </div>
+    </section>
+  );
 }
